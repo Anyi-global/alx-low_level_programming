5dd94426d6f8db3ab14a2eccde8a1b541df332c3
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-03-17 16:26:38 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints base 16 numbers in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	
+	for (i = 0; i <= 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
