45dabdee12d51900920aea9dfe64a22c6ab56884
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-03-17 16:13:39 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowercase alphabets in reverse order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c >= 'z'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
