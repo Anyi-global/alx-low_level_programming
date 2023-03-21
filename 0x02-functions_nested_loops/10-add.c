8258af1b041bc3389e22a95032c8052e697bde7c
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-03-21 16:37:33 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - A function that adds two integer numbers
+ * @a: The first parameter to be computed
+ * @b: The second parameter to be computed
+ * Return: returns an integer value
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
