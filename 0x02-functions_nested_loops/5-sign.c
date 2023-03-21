1ccebedd6b986efe2d9141e173e9a233e38e5419
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-03-21 14:30:31 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - A function that prints the sign of a number
+ * @n: The integer to be checked
+ * Return: 1 for negative num, -1 for negative num, or 0 for anything else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
