24a60a89dfd23bcba2985634de5261d5c141fa91
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-03-27 16:56:22 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = 0;
 
-	while (*(str + i) != '\0')
+	while (str[i++])
 	{
-		_putchar(*(str + i));
-		i += 2;
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
