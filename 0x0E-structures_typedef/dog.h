06ad02c58851ee42d18ba2c7162fd70ec27af739
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-04-17 13:14:17 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct structure that holds dog elements
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
