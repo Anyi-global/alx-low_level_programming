651509a9c7e3f02b107ca0c555ccfd583f4557a2
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-04-20 02:14:02 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,18 +4,18 @@
 * main - a program that prints the opcodes of it's main function
 * @argc: number of arguments
 * @argv: argument vector
-* Return: returns the opcodes
+* Return: Always 0 (Success)
 */
 
 int main(int argc, char **argv)
 {
 	int nbytes, i;
-	char *address;
+	char *addr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
 	nbytes = atoi(argv[1]);
@@ -23,16 +23,20 @@ int main(int argc, char **argv)
 	if (nbytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	address = (char *)&main;
+	addr = (char *)&main;
 
 	for (i = 0; i < nbytes; i++)
 	{
-		printf("%02hhx ", address[i]);
+		if (i == nbytes - 1)
+		{
+			printf("%02hhx\n", addr[i]);
+			break;
+		}
+		printf("%02hhx ", addr[i]);
 	}
-	printf("\n");
 
 	return (0);
 }
