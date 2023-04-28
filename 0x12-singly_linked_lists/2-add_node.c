d381ed1aada54effa1a87d8eb0cd612275798bfb
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-04-28 02:52:29 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,16 +13,19 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (str == NULL){
+	if (str == NULL)
+	{
 		return (NULL);
 	}
 	new_node = malloc(sizeof(list_t));
-	
-	if (new_node == NULL){
+
+	if (new_node == NULL)
+	{
 		return (NULL);
 	}
 	new_node->str = strdup(str);
-	if (new_node->str == NULL){
+	if (new_node->str == NULL)
+	{
 		return (NULL);
 	}
 	new_node->len = strlen(str);
