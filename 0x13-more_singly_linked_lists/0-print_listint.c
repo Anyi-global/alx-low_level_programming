8c126ca8c50b5b1ff453a43aadb02808d3960437
Anyi-global <ikeifeanyi95.ii@gmail.com>
2023-05-01 01:47:13 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes_num;
+	size_t nodes_count;
 
-	nodes_num = 0;
+	nodes_count = 0;
 	while (h)
 	{
 		printf("%d\n", h->n);
-		nodes_num++;
+		nodes_count++;
 		h = h->next;
 	}
-	return (nodes_num);
+	return (nodes_count);
 }
